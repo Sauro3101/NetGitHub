ba225c84f51bc355675d388738e9b95bfe00502e
Sauro3101 <91719731+Sauro3101@users.noreply.github.com>
2021-10-29 21:32:18 +0200

    concessionario work

--- Concessionario/src/concessionario/Concessionario.java
+++ Concessionario/src/concessionario/Concessionario.java
@@ -40,7 +40,18 @@ public class Concessionario {
     public static void setMaxAuto(int maxAuto) {
         Concessionario.maxAuto = maxAuto;
     }
+    
+    public void iniAuto(){
+        auto = java.util.Arrays.copyOf(auto, auto.length +4);
+        auto[0]= new Auto("Ferrari","488 Pista",2,"Rosso",8,3902,720,770);
+        auto[1]= new Auto("Lamborghini","Huracan STO",2,"Azzurro",10,5204,640,565);
+        auto[2]= new Auto("Ferrari","F8 Tributo",2,"Rosso",8,3902,720,770);
+        auto[3]= new Auto("Lamborghini","Aventador SVJ",2,"Verde",12,6498,770,720);
+    }
     public void addAuto(){
+        ConsoleInputConException in = new ConsoleInputConException();
+        int posti,cilindri,cilindrata,potenza,coppia;
+        String marca,modello,colore;
         System.out.println("Inserisci i dati dell'auto:\nMarca:");
         marca=in.readLine();
         System.out.println("Modello:");
@@ -57,19 +68,29 @@ public class Concessionario {
         potenza=in.readInt();
         System.out.println("Coppia(nm):");
         coppia=in.readInt();
-        Auto[nAuto] = new Auto();
+        auto = java.util.Arrays.copyOf(auto, auto.length +1);
+        auto[nAuto] = new Auto(marca,modello,posti,colore,cilindri,cilindrata,potenza,coppia);
         nAuto++;
     }
-    public void ordinaMarca(){
+    public String findColore(String colore){
+        for(int i=0;i<nAuto;i++){
+            if(colore.equals(auto[i].getColore())){
+                return auto[i].toString();
+            }
+        }
+        return "ERROR";
+    }
+    public Auto[] ordinaMarca(){
         Auto tmp;
         for (int i = 0; i < auto.length; i++){  
             for (int j = i + 1; j < auto.length; j++){
-                if (auto[i].compareToIgnoreCase(auto[j])>0){
+                if (auto[i].getMarca().compareToIgnoreCase(auto[j].getMarca())>0){
                     tmp = auto[i];
                     auto[i] = auto[j];  
                     auto[j] = tmp;
                 }
             }
         }
+        return auto;
     }
 }
--- Concessionario/src/concessionario/main.java
+++ Concessionario/src/concessionario/main.java
@@ -4,8 +4,6 @@
  */
 package concessionario;
 
-import java.util.Arrays;
-
 /**
  *
  * @author Mattia Checchetto
@@ -17,113 +15,43 @@ public class main {
      */
     public static void main(String[] args) {
         ConsoleInputConException in = new ConsoleInputConException();
-        int scelta;
+        int scelta,i;
         String findColore;
-        int i,numeroAuto,posti,cilindri,cilindrata,potenza,coppia;
-        String marca,modello,colore;
         
-        System.out.println("INIZIALIZZAZIONE CONCESSIONARIO:\nInserire il numero di auto che si intende memorizzare(min 1-max 15):");
-        numeroAuto=in.readInt();
-        Auto[] b= new Auto[numeroAuto];
-        Concessionario con= new Concessionario(numeroAuto);
-        b[0]= new Auto("Ferrari","488GTB",2,"Rosso",8,4300,800,750);
-        b[1]= new Auto("Lamborghini","Huracan STO",2,"Azzurro",10,6500,700,750);
-        b[2]= new Auto("Ferrari","F8 Tributo",2,"Rosso",8,4300,820,770);
-        for(i=3;i<numeroAuto;i++){
-            System.out.println("Inserisci i dati dell'auto:\nMarca:");
-            marca=in.readLine();
-            System.out.println("Modello:");
-            modello=in.readLine();
-            System.out.println("Numero di posti:");
-            posti=in.readInt();
-            System.out.println("Colore:");
-            colore=in.readLine();
-            System.out.println("Dati motore:\nNumero di cilindri");
-            cilindri=in.readInt();
-            System.out.println("Cilindrata:");
-            cilindrata=in.readInt();
-            System.out.println("Potenza(cv):");
-            potenza=in.readInt();
-            System.out.println("Coppia(nm):");
-            coppia=in.readInt();
-            b[i]= new Auto(marca,modello,posti,colore,cilindri,cilindrata,potenza,coppia);
-            
-        }
-        con.setAuto(b);
-        Auto[] a=con.getAuto();
-        System.out.println(a[1].toString()+"\n\n");
-        
+        System.out.println("INIZIALIZZAZIONE CONCESSIONARIO:\nSono state inizializzate 4 auto");
+        Concessionario con= new Concessionario(4);
+        Auto[] a= new Auto[con.getnAuto()];
+        con.iniAuto();
         do{
-            System.out.println("Digitare il numero corrispondente all'opzione scelta:\n 1. Inserire una nuova auto\n" +
-            "2. Visualizzare tutte le auto\n" +
-            "3. Visualizzare i dati della prima auto che abbia il colore passato per parametro\n" +
-            "4. Visualizzare i dati di  tutte le auto del concessionario ordinate per marca\n0. Exit");
+            System.out.println("\n\nDigitare il numero corrispondente all'opzione scelta:\n" + "1. Inserire una nuova auto\n" + "2. Visualizzare tutte le auto\n" + "3. Visualizzare i dati della prima auto che abbia il colore passato per parametro\n" + "4. Visualizzare i dati di  tutte le auto del concessionario ordinate per marca\n" + "0. Exit");
             scelta=in.readInt();
             
             switch(scelta){
-                case 0:
-                    break;
-                case 1:
-                    //Inserire una nuova auto
-                    System.out.println("Inserisci i dati dell'auto:\nMarca:");
-                    marca=in.readLine();
-                    System.out.println("Modello:");
-                    modello=in.readLine();
-                    System.out.println("Numero di posti:");
-                    posti=in.readInt();
-                    System.out.println("Colore:");
-                    colore=in.readLine();
-                    System.out.println("Dati motore:\nNumero di cilindri");
-                    cilindri=in.readInt();
-                    System.out.println("Cilindrata:");
-                    cilindrata=in.readInt();
-                    System.out.println("Potenza(cv):");
-                    potenza=in.readInt();
-                    System.out.println("Coppia(nm):");
-                    coppia=in.readInt();
-                    numeroAuto++;
-                    con.setnAuto(numeroAuto);
-                    b = java.util.Arrays.copyOf(b, b.length +1);
-                    b[con.getnAuto()-1]= new Auto(marca,modello,posti,colore,cilindri,cilindrata,potenza,coppia);
-                    con.setAuto(b);
-                    a=con.getAuto();
-                    break;
-                case 2:
+                case 0 -> {
+                }
+                case 1 -> //Inserire una nuova auto
+                    con.addAuto();
+                case 2 -> {
                     //Visualizzare tutte le auto
+                    a=con.getAuto();
                     for(i=0;i<con.getnAuto();i++){
                         System.out.println(a[i].toString());
                     }
-                    break;
-                case 3:
+                }
+                case 3 -> {
                     //Visualizzare i dati della prima auto che abbia il colore passato per parametro
-                    System.out.println("Colore da cercare:");
+                    System.out.println("\n\nColore da cercare:");
                     findColore=in.readLine();
-                    for(i=0;i<con.getnAuto();i++){
-                        if(findColore.equals(a[i].getColore())){
-                            System.out.println(a[i].toString());
-                            i=con.getnAuto();
-                        }
-                    }
-                    break;
-                case 4:
+                    System.out.println(con.findColore(findColore));
+                }
+                case 4 -> {
                     //Visualizzare i dati di  tutte le auto del concessionario ordinate per marca
-                    Auto tmp;
-                    for (i = 0; i < a.length; i++){  
-                        for (int j = i + 1; j < a.length; j++){
-                            if (a[i].compareToIgnoreCase(a[j])>0){
-                                tmp = a[i];
-                                a[i] = a[j];  
-                                a[j] = tmp;
-                            }
-                        }
-                    }
+                    a=con.ordinaMarca();
                     for(i=0;i<con.getnAuto();i++){
-                        System.out.println(a[i].toString());
+                        System.out.println(a[i].toString()+"\n\n");
                     }
-                    break;
-                default:
-                    System.out.println("ERRORE");
-                    break;
+                }
+                default -> System.out.println("ERRORE\n");
             }
         }while(scelta!=0);
         
